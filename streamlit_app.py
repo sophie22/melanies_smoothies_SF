86d8045c049d3cba7ff1daef81f2de090d9b088e
Sophie R <sophie22@users.noreply.github.com>
2024-12-09 22:10:09 +0000

    Update streamlit_app.py
    update buttons and input fields

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,14 @@ st.write(
     """
 )
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on you Smoothie will be", name_on_order)
+
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe)
+ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
 if ingredients_list:
     ingredients_string = ''
@@ -25,10 +27,11 @@ if ingredients_list:
 
     # st.write(ingredients_string)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     # st.write(my_insert_stmt)
+    # st.stop()
     time_to_insert = st.button('Submit Order!')
 
     
